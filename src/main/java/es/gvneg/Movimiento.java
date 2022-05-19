43672794e39abbe9603aa791ae8aa06e2b89545a
DIglesias <danygle86@gmail.com>
2022-05-19 05:49:32 +0200

    Modificando tipo fecha Movimiento a LocalDate

--- src/main/java/es/gvneg/Movimiento.java
+++ src/main/java/es/gvneg/Movimiento.java
@@ -1,34 +1,34 @@
 package es.gvneg;
 
-import java.time.LocalDateTime;
+import java.time.LocalDate;
 
 public abstract class Movimiento {
 	
 	private String concepto;
 	private float importe;
-	private LocalDateTime fecha;
+	private LocalDate fecha;
 	
 	public String getConcepto() {
 		return concepto;
-	}
-	
-	public void setConcepto(String concepto) {
-		this.concepto = concepto;
 	}
 	
 	public float getImporte() {
 		return importe;
 	}
 	
+	public LocalDate getFecha() {
+		return fecha;
+	}
+	
+	public void setConcepto(String concepto) {
+		this.concepto = concepto;
+	}
+	
 	public void setImporte(float importe) {
 		this.importe = importe;
 	}
 	
-	public LocalDateTime getFecha() {
-		return fecha;
-	}
-	
-	public void setFechaDateTime(LocalDateTime fecha) {
+	public void setFecha(LocalDate fecha) {
 		this.fecha = fecha;
 	}
 	
@@ -36,7 +36,7 @@ public abstract class Movimiento {
 		super();
 	}
 	
-	public Movimiento(String concepto, float importe, LocalDateTime fecha) {
+	public Movimiento(String concepto, float importe, LocalDate fecha) {
 		this.concepto = concepto;
 		this.importe = importe;
 		this.fecha = fecha;
